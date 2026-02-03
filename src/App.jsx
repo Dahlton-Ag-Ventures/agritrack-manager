4a6e78b09770ba78f7c75c3582a6069d61bd3730
Dahlton-Ag-Ventures <invoice.ssi.lofi@gmail.com>
2026-02-03 14:57:55 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const themes = {
   dark: {
     background: '#0a0a0a',
     backgroundImage: window.innerWidth < 768 
-  ? 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://agritrack-manager.vercel.app/Mobile-friendly_John_Deere_Starry_Night.png")'
+  ? 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://agritrack-manager.vercel.app/john_deere_high_res.jpg")'
   : 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://agritrack-manager.vercel.app/John_Deere_Combine_App_background.jpg")',
     backgroundSize: window.innerWidth < 768 ? 'cover' : 'cover',
     backgroundPosition: window.innerWidth < 768 ? 'center center' : 'center',
